a1552c0511ab53abc1fb93d5ffed9db00027a1be
vineet rajput <vineetkumar013>
2023-08-15 16:21:58 +0530

    first commit

---
+++ Routes/authRoute.js
@@ -0,0 +1,27 @@
+const {
+  createUser,
+  loginUser,
+  verifyOTP,
+  getAllUser,
+  getByUser,
+  deleteUser,
+  updateUser,
+  resendOTP,
+  ForgetPassword,
+} = require("../Controller/authCtrl");
+const Admin = require("../Middleware/admin");
+const verifyToken = require("../Middleware/verifyToken");
+
+const router = require("express").Router();
+
+router.post("/create", createUser);
+router.post("/login", loginUser);
+router.post("/verify-otp/:id", verifyOTP);
+router.get("/user"  , getAllUser);
+router.get("/user/:id", verifyToken, getByUser);
+router.put("/user/:id", verifyToken, updateUser);
+router.delete("/user/:id", /* Admin, */ deleteUser);
+router.post("/resendOtp/:id", resendOTP);
+router.post("/forget", ForgetPassword);
+
+module.exports = router;
